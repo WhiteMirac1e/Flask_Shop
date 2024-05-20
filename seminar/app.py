b20b3da435bbcd68a122467777bbfc54ddad073e
Konnov Ivan <konnov-ivan@yandex.ru>
2024-05-20 11:50:42 +0300

    third commit

--- seminar/app.py
+++ seminar/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, make_response, redirect, url_for
+from seminar.forms import RegistrationForm
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
+
+# app.config['SECRET_KEY'] = b'49d45b3a56115f41aadaca10825fd35ab95d1b95ddad1293b4806b51e53dc755'
+# csrf = CSRFProtect(app)
 
 
 @app.route('/')
@@ -27,5 +33,24 @@ def shoes():
     return render_template('shoes.html', **context)
 
 
+# @app.route('/register/', methods=['GET', 'POST'])
+# def register():
+#     response = make_response(render_template('hello.html'))
+#     response.set_cookie('username', 'admin')
+#     return response
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        context = {'username': username}
+        response = make_response(render_template('hello.html', **context))
+        response.set_cookie('username', username)
+        return response
+        # return f'Hello {username}!'
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from seminar.app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
